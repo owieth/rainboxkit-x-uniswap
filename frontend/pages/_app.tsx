3aa71808fa1fd345c751b4d2df23c58aa905e931
owieth <60756360+owieth@users.noreply.github.com>
2022-07-27 14:51:21 +0200

    feat(example): use uniswap widget with rainbowkit or web3modal

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -10,7 +10,7 @@ import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.goerli, chain.rinkeby, chain.polygon, chain.polygonMumbai, chain.optimism, chain.arbitrum],
+  [chain.goerli],
   [
     alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
     publicProvider()
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,61 +1,120 @@
-import { Col, Container, Grid, Text } from '@nextui-org/react';
+import { Button, Container, Grid, Text } from '@nextui-org/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import Spline from '@splinetool/react-spline';
+import { SwapWidget } from '@uniswap/widgets';
+import '@uniswap/widgets/fonts.css';
+import { ethers } from "ethers";
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
+import { useAccount, useSigner } from 'wagmi';
+import Web3Modal from "web3modal";
+
+const providerOptions = {
+  walletlink: {
+    package: '', // Required
+    options: {
+      appName: "Web 3 Modal Demo", // Required
+      infuraId: process.env.INFURA_KEY // Required unless you provide a JSON RPC url; see `rpc` below
+    }
+  },
+  walletconnect: {
+    package: '', // required
+    options: {
+      infuraId: process.env.INFURA_KEY // required
+    }
+  }
+}
+
+let web3Modal: Web3Modal;
+if (typeof window !== 'undefined') {
+  web3Modal = new Web3Modal({
+    providerOptions, // required
+  })
+}
 
 const Home: NextPage = () => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => setMounted(true), []);
 
+  // Rainbowkit
+  const { data: signer } = useSigner()
+  const { address } = useAccount()
+
+  // Web3Modal
+  const [provider, setProvider] = useState();
+  const [account, setAccount] = useState("");
+
+
+  const connectWallet = async () => {
+    try {
+      const provider = await web3Modal.connect();
+      const library = new ethers.providers.Web3Provider(provider);
+      const accounts = await library.listAccounts();
+      setProvider(provider);
+      if (accounts) setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  const disconnect = async () => {
+    await web3Modal.clearCachedProvider();
+    setAccount("");
+  };
+
+  const renderUniswapWidgetRainbow = () => {
+    return (
+      <Grid.Container gap={2}>
+        <Grid xs={6}>
+          <SwapWidget
+            provider={signer?.provider as ethers.providers.JsonRpcProvider}
+          />
+        </Grid>
+      </Grid.Container>
+    )
+  }
+
+  const renderUniswapWidgetWeb3Modal = () => {
+    return (
+      <Grid.Container gap={2}>
+        <Grid xs={6}>
+          <SwapWidget
+            provider={provider}
+          />
+        </Grid>
+      </Grid.Container>
+    )
+  }
+
   return (
     <>
       <Container display='flex' justify='center' style={{ height: '100vh' }} gap={5}>
         <Grid.Container gap={2} alignItems='center'>
           <Grid xs={9}>
-            <Text h1>Hi 👋, start your DApp here ➡️</Text>
+            <Text h1>🌈 Rainbowkit 🤝 Uniswap 🦄</Text>
           </Grid>
           <Grid xs={3} justify='flex-end'>
             <ConnectButton />
           </Grid>
         </Grid.Container>
 
-        <Grid.Container gap={2}>
-          <Grid xs={6} alignItems='center'>
-            <Col>
-              <Text
-                h1
-                css={{
-                  textGradient: "45deg, $blue600 -20%, $pink600 50%",
-                }}
-                weight="bold"
-              >
-                NextJS
-              </Text>
-              <Text
-                h1
-                css={{
-                  textGradient: "45deg, $purple600 -20%, $pink600 100%",
-                }}
-                weight="bold"
-              >
-                RainbowKit
-              </Text>
-              <Text
-                h1
-                css={{
-                  textGradient: "45deg, $yellow600 -20%, $red600 100%",
-                }}
-                weight="bold"
-              >
-                Template
-              </Text></Col>
+        <Grid.Container gap={2} alignItems='center'>
+          <Grid xs={9}>
+            <Text h1>👛 Web3Modal 🤝 Uniswap 🦄</Text>
           </Grid>
-          <Grid xs={6}>
-            {mounted && <Spline height={'700px'} width={'700px'} scene="https://prod.spline.design/JjyZ2JWcJGEegHzW/scene.splinecode" />}
+          <Grid xs={3} justify='flex-end'>
+            {!account ? (
+              <Button onClick={connectWallet}>Connect Wallet</Button>
+            ) : (
+              <Button onClick={disconnect}>Disconnect</Button>
+            )}
           </Grid>
         </Grid.Container>
+
+        <>
+          {mounted && address && renderUniswapWidgetRainbow()}
+          {mounted && account && renderUniswapWidgetWeb3Modal()}
+        </>
       </Container>
     </>
   )
